1d0e070f7089c54595fcb7927b4707f848f6ffaf
Aaditi17 <deepaliaj3@gmail.com>
2021-05-14 17:03:38 -0700

    Slingshot

--- sketch.js
+++ sketch.js
@@ -1,10 +1,13 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var slingshot;
+
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,8 +36,11 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-
+   // log6 = new Log(200,180,80,PI/2);
+    
     bird = new Bird(100,100);
+    slingshot = new Slingshot(bird.body,{x:200, y:100});
+    
 
 }
 
@@ -58,7 +64,20 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
+    //log6.display();
+    
+   
+
 
     bird.display();
+    slingshot.display();
     platform.display();
+}
+
+function mouseDragged(){
+   Matter.Body.setPosition(bird.body, {x: mouseX, y: mouseY});
+}
+
+function mouseReleased(){
+    slingshot.fly();
 }
